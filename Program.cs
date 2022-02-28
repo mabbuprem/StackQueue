1613f6608411b6f50fed89c36a2e55186b8c39eb
mabbuprem <mabbupremkumar@gmail.com>
2022-02-28 15:53:57 +0530

    create stack

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ namespace StackQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Stacks and Queues");
-          
+            Stack stack = new Stack();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+            stack.DisplayStack();
 
         }
     }
