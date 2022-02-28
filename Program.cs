bc266c71708e8726fd4857f06cfaa1df24f0ad5f
mabbuprem <mabbupremkumar@gmail.com>
2022-02-28 15:50:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stacks and Queues");
+          
+
+        }
+    }
+}
